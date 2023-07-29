de3d6137ab8a8e18ec67a40ab27887637ca0e0c3
Joseph-Ajegetina <josephajongbah@gmail.com>
2023-07-29 15:38:11 +0000

    addded seed data for home view v2

--- models/Bucket.model.js
+++ models/Bucket.model.js
@@ -3,11 +3,11 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const bucketSchema = new Schema(
   {
-    resources: [{type: mongoose.Schema.Types.ObjectId, ref: 'Resource'}],
+    resources: [{type: Schema.Types.ObjectId, ref: 'Resource'}],
     name: String,
     description: String,
-    upVote: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    downVote: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    upVote: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    downVote: [{type: Schema.Types.ObjectId, ref: 'User'}],
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
